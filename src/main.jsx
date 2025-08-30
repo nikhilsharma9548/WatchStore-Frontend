51d2f0eecfad4ba707a7a311446508210ee7f240
nikhilsharma9548 <ns630403@gmail.com>
2025-08-30 10:37:30 +0530

    Fix case sensitive import for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom';
-import { AppContextProvider } from './Context/AppContext';
+import { AppContextProvider } from './Context/AppContext.jsx';
+
 
 
 createRoot(document.getElementById('root')).render(

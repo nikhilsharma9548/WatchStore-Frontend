5356a43457dd11d94e00e7f25953cf4ed73b789a
nikhilsharma9548 <ns630403@gmail.com>
2025-09-02 15:16:23 +0530

    done

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
 
       <div className=" flex flex-col gap-5  items-center justify-center">
         <div className=" border-5  aspect-square w-14 rounded-full
-         border-blue-600 border-t-white border-r-0 border-l-0
+         border-blue-600 border-t-black/40 border-r-0 border-l-0
           animate-spin">
         </div>
         <p  className='font-sans text-xl'>Loading....</p>

1f122d4ce5456cb44ad03ab0a917ae8139b62b60
nikhilsharma9548 <ns630403@gmail.com>
2025-09-02 15:26:12 +0530

    sure

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='inset-0 z-50 bg-black/40 min-h-screen items-center 
+    <div className='inset-0 z-50 bg-white/40 min-h-screen items-center 
     flex justify-center w-full'>
 
       <div className=" flex flex-col gap-5  items-center justify-center">
         <div className=" border-5  aspect-square w-14 rounded-full
-         border-blue-600 border-t-black/40 inset-0  border-r-0 border-l-0
+         border-blue-600 border-t-white/40 inset-0  border-r-0 border-l-0
           animate-spin">
         </div>
         <p  className='font-sans text-xl'>Loading....</p>

4427a419d19aafb489bcf0f4faea0ed49149a138
nikhilsharma9548 <ns630403@gmail.com>
2025-09-04 00:00:59 +0530

    ok

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -108,7 +108,7 @@ const logout = async() =>{
                     <img src={assets.profile} className='w-10 h-10 rounded-full' />
 
                     <div className='hidden group-hover:block h-96 w-60 p-5 bg-white  absolute top-10 right-0'>
-                      <img src={assets.profile} className='w-14 h-14 rounded-full pb-5' />
+                      <img src={assets.profile} className='w-14 h-14 rounded-full mb-5' />
                       <ul className=' px-1.5 
                      shadow border border-gray-700 py-2.5 rounded-md text-sm z-40'>
                       <li onClick={() => navigate('/my-orders')} className='p-1.5 hover:bg-pink-700/10 cursor-pointer'>My Orders</li>

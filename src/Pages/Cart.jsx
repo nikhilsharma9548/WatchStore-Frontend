fa9d0cd5c09653a87536f0c18420c6d40512b21f
nikhilsharma9548 <ns630403@gmail.com>
2025-09-03 23:36:16 +0530

    ok

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -35,7 +35,6 @@ const Cart = () => {
     const getUserAddress = async() =>{
         try {
             const{ data } = await axios.get('/api/address/get');
-
             if(data.success){
                 setAddresses(data.addresses)
                 if(data.addresses.length > 0){

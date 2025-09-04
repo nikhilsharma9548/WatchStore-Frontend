1700fddf4cc2a5dd6826c8ee56d52062f770b461
nikhilsharma9548 <ns630403@gmail.com>
2025-09-04 11:42:55 +0530

    ok

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -8,16 +8,14 @@ const Features = () => {
 
     return (
         <>
-        {!loading ? (<div id='features' className='sm:px-10 px-5 scroll-mt-24'>
+    <div id='features' className='sm:px-10 px-5 scroll-mt-24'>
             <p className='text-3xl w-32 border-b-2'>Features</p>
             <div className='py-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 '>
-
                 {products.filter((product) =>product.inStock).slice(0,5).map((product, index) =>(
                      <ProductCard key={index} product={product}/>
                 ))}
-
             </div>
-        </div>) :<Loading/>}
+    </div>)
         
         </>
     );

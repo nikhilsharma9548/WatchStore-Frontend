914b493feff7836b108b35211702baa37fc9ee45
nikhilsharma9548 <ns630403@gmail.com>
2025-09-07 00:05:39 +0530

    ok

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -10,8 +10,8 @@ const Loading = () => {
     flex justify-center w-full min-h-screen '>
 
       <div className=" flex flex-col gap-5  items-center justify-center">
-        <div className=" border-2  aspect-square w-14 rounded-full
-         border-t-white   border-gray-200 inset-0
+        <div className=" border-4  aspect-square w-14 rounded-full
+         border-t-white   border-gray-300 inset-0
           animate-spin">
         </div>
       </div>

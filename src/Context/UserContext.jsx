e1b991ba0ed1e01e28a66301a10184d9efca0275
nikhilsharma9548 <ns630403@gmail.com>
2025-09-08 22:07:39 +0530

    ok

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,4 +1,7 @@
 import { createContext, useContext, useState } from "react";
+import axios from 'axios'
+import toast from "react-hot-toast";
+
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL

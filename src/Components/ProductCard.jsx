e3d8f27eca3155fd2a03e523aae8f921706278e8
nikhilsharma9548 <ns630403@gmail.com>
2025-09-12 22:48:16 +0530

    ok

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -18,7 +18,10 @@ const ProductCard = ({product}) => {
          initial ={{opacity:0, scale:0.7 }}
             whileInView={{opacity:1,scale:1}}
             transition={{duration:0.7}}
-        onClick={() => {navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scrollTo(0,0)}}  className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 flex justify-between flex-col bg-white  w-full shadow-lg shadow-gray-700">
+        onClick={() => {navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scrollTo(0,0)}}
+          className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 flex justify-between
+           flex-col bg-white  w-full shadow-lg shadow-gray-700">
+
             <div className="group cursor-pointer flex items-center justify-center px-2">
                 <img className="group-hover:scale-105 transition max-w-24 md:max-w-36" src={product.image[0]} alt={product.name} />
             </div>
@@ -28,19 +31,19 @@ const ProductCard = ({product}) => {
                 <div className="flex items-center gap-0.5">
                     {Array(5).fill('').map((_, i) => (
                          4 > i ? (
-                            <p key={i} className="text-pink-700"><MdStarRate/></p>
+                            <p key={i} className="text-[#274472]"><MdStarRate/></p>
                         ) : (
-                            <p key={i} className="text-pink-300/80"><MdStarRate/></p>
+                            <p key={i} className="text-[#274472]/40"><MdStarRate/></p>
                         )
 
                     ))}
                     <p>({4})</p>
                 </div>
                 <div onClick={(e) =>{e.stopPropagation(); }} className="flex items-end justify-between mt-3">
-                    <p className="md:text-xl text-base font-medium text-pink-700">
+                    <p className="md:text-xl text-base font-medium text-[#274472]">
                         {currency}{product.offerPrice} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}{product.price}</span>
                     </p>
-                    <div className="text-pink-700">
+                    <div className="text-[#274472]">
                         {!cartItems[product._id] ? (
                             <button className="flex items-center justify-center gap-1 bg-pink-100 border border-pink-300 md:w-[80px] w-[64px] h-[34px] rounded text-pink-600 font-medium cursor-pointer" onClick={() => addToCart(product._id)} >
                                 <p><IoCartOutline/></p>

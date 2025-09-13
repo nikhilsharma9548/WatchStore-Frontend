11ed2ba5e2c21b39c7975b2edad3233f898c7b67
nikhilsharma9548 <ns630403@gmail.com>
2025-09-13 17:54:07 +0530

    ok

--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -30,7 +30,7 @@ const AllProducts = () => {
           <div className='w-16  h-0.5 bg-[#274472] rounded-full'></div>
         </div>
 
-         <div className="lg:hidden justify-end max-w-96 mt-5 flex items-center text-base gap-2 border-2 border-gray-800 px-3 rounded-full">
+         <div className="lg:hidden justify-self-end max-w-96 mt-5 flex items-center text-base gap-2 border-2 border-gray-800 px-3 rounded-full">
                           <input
                           value={searchQuery} 
                           onChange={(e)=> setSearchQuery(e.target.value)} className="py-2.5 w-full bg-transparent  outline-none px-3 " type="text" placeholder="Search products" />

506b50940d63ba42d25033ea4e75855e7fe131c6
nikhilsharma9548 <ns630403@gmail.com>
2025-09-13 22:31:39 +0530

    push

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets';
 const Loading = () => {
     return (
     <div className='inset-0 z-50 items-center 
-    flex justify-center w-full min-h-screen '>
+    flex justify-center w-full min-h-screen bg-[#C3E0E5]'>
 
       <div className=" flex flex-col gap-5  items-center justify-center">
         <div className=" border-4  aspect-square w-14 rounded-full

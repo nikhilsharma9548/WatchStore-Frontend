e238816ea5ae66b57cca6aebc917fc8973801b0a
nikhilsharma9548 <ns630403@gmail.com>
2025-09-13 22:06:58 +0530

    ok

--- src/Components/Load.jsx
+++ src/Components/Load.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../Context/AppContext";
 
-const Load = () => {
-  const { showSplash, setShowSplash } = useAppContext();
+ const Load = () => {
+ const { showSplash, setShowSplash } = useAppContext();
 
   useEffect(() => {
     const alreadyShown = localStorage.getItem("splashShown");
@@ -22,14 +22,21 @@ const Load = () => {
     }
   }, [setShowSplash]);
 
-  if (!showSplash) return null; // splash off hone par kuch render na karo
-
+  if (!showSplash) return null; 
+  
   return (
-    <div className="fixed bg-[#C3E0E5] flex items-center justify-center z-50 animate-fade">
-      <img src={assets.logo} className="h-10 w-10 rounded-full" alt="" />
-      <h1 className="text-4xl font-bold text-gray-900">TimeAura</h1>
+    <div className="relative min-h-screen bg-[#C3E0E5] flex items-center justify-center z-50">
+      {/* Splash Screen */}
+      {showSplash && (
+        <div className="fixed inset-0 bg-[#C3E0E5] flex items-center justify-center z-50 animate-fade">
+            <img src={assets.logo} className=' h-10 w-10 rounded-full' alt="" />
+          <h1 className="text-4xl font-bold text-gray-900 ">
+            TimeAura
+          </h1>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Load;

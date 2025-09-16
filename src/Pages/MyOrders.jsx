504358155e8bf6087f9194099a26b2013c51592d
nikhilsharma9548 <ns630403@gmail.com>
2025-09-16 23:25:36 +0530

    fine

--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -99,7 +99,7 @@ const MyOrders = () => {
                         <p className='text-green-400 text-xl'>Amount: {currency}{item.product.offerPrice * item.quantity}</p>
                     </div>
                 ))}
-                <button  onClick={() => cancelOrder(order._id)}
+                <button 
                 className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg mt-2">Cancel</button>
             </div>
 

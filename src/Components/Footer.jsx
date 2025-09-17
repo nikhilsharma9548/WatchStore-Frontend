8b0f7f35e8299c113f1ef8bec47a0cf6e213acde
nikhilsharma9548 <ns630403@gmail.com>
2025-09-17 18:46:48 +0530

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,59 +3,64 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import {NavLink} from 'react-router-dom'
+import { FiLinkedin } from "react-icons/fi"
+import { BsTwitterX } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { assets } from '../assets/assets';
+
 const Footer = () => {
   return (
-    <>
-  <div className={`w-full md:mb-0 mb-16 `}>
-
-      <div className='bg-[#274472] flex max-md:flex-col justify-center max-sm:items-center
-     md:justify-between max-sm:gap-5 text-gray-300 md:px-20 md:py-10 p-3 mt-32 gap-5'>
-
-        <div className=' flex flex-col justify-center items-center gap-3'>
-            <p className='font-semibold border-b-2'>Our Information</p>
-           <div className='flex md:flex-col gap-3'>
-            <p className='text-sm'>Badaun</p>
-            <p className='text-sm'>+91 123 456 7890</p>
-            <p className='text-sm'>nikhilsharma9533@gmail.com</p>
-            <p className='text-sm'></p>
-           </div>
-        </div>
-
-        <div className='flex flex-col justify-center items-center gap-3'>
-            <p className='font-semibold border-b-2'>About Us</p>
-            <div className='flex gap-3 md:flex-col'>
-            <p className='text-sm'>Contact Support</p>
-            <p className='text-sm'>Privacy Policy</p>
-            <p className='text-sm'>About Us</p>
-            <p className='text-sm'>CopyRight</p>
+        <footer className="px-6 md:px-16 lg:px-24 xl:px-32 w-full bg-[#274472] max-md:mb-16">
+            <div className="flex flex-col md:flex-row items-start justify-center gap-10 py-10 border-b border-gray-200">
+                
+                <div className="max-w-96">
+                   <div className='flex items-center gap-2 text-2xl font-bold text-white                                                                          '>
+                     <img src={assets.logo} className='w-10' />
+                    <span>TimeAura</span>
+                   </div>
+                    <p className="mt-6 text-sm text-gray-200 pb-2">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.
+                    </p>
+                    <div className="flex items-center gap-3 mt-3 text-white ">
+                        <a href="#">
+                           <p className='hover:-translate-y-1 duration-150'><BsTwitterX/></p>
+                        </a>
+                        <a href="#">
+                            <p className='text-xl hover:-translate-y-1 duration-150'><FaGithub/></p>
+                        </a>
+                        <a href="#">
+                            <p className='text-xl hover:-translate-y-1 duration-150'><FiLinkedin/></p>
+                        </a>
+                    </div>
+                </div>
+        
+                <div className="w-1/2 flex flex-wrap md:flex-nowrap justify-between">
+                    <div>
+                        <h2 className="font-semibold text-gray-300 mb-5">RESOURCES</h2>
+                        <ul className="text-sm text-gray-50 space-y-2 list-none">
+                            <li><a href="#">Documentation</a></li>
+                            <li><a href="#">Tutorials</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Community</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className="font-semibold text-gray-300 mb-5">COMPANY</h2>
+                        <div className="text-sm text-gray-50 space-y-2 list-none">
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Terms</a></li>
+                        </div>
+                    </div>
+                </div>
+        
             </div>
-        </div>
-
-        <div className=' flex flex-col justify-center items-center gap-3'>
-            <p className='font-semibold border-b-2'>Product</p>
-            <div className='flex md:flex-col gap-3'>
-            <p className='text-sm'>Luxury</p>
-            <p className='text-sm'>Causal</p>
-            <p className='text-sm'>Electric</p>
-            <p className='text-sm'>Custom</p>
-            </div>
-        </div>
-
-        <div className='flex flex-col gap-3 items-center'> 
-            <p className='font-semibold border-b-2'>social Media</p>
-            <div className='flex gap-3 text-2xl'>
-                <NavLink to={'/'}><FaSquareXTwitter/></NavLink>
-                <NavLink to={'/'}><FaLinkedin/></NavLink>
-                <NavLink to={'https://www.instagram.com/sharmaharrdy'}><FaSquareInstagram/></NavLink>
-            </div>
-        </div>
-    </div>
-
-            <div className='flex justify-center bg-[#043053] p-4 text-white '>@ All right reserved  || Nikhil Sharma</div>
-
-  </div>
-    </>
-  )
+            <p className="py-4 text-center text-xs md:text-sm text-gray-50">
+                Copyright 2024 © Nikhil Shamra. All Right Reserved.
+            </p>
+        </footer>
+    );
 }
 
 export default Footer

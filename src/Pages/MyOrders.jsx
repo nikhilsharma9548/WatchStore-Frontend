160a53ae80840041be7ee725fa1d7065399b59ce
nikhilsharma9548 <ns630403@gmail.com>
2025-09-17 17:04:05 +0530

    fine

--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -98,7 +98,7 @@ const MyOrders = () => {
                             {/* Cancel Button */}
                             {order.status !== "cancelled" && (
                                 <button
-                                    onClick={() => cancelOrder(order._id)}
+                                      onClick={() => cancelOrder(order._id)}
                                     className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg mt-2">
                                     Cancel
                                 </button>

4228124abd168eee6999763e6480cecb73d90b8a
nikhilsharma9548 <ns630403@gmail.com>
2025-09-20 16:25:31 +0530

    as soon

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -52,7 +52,7 @@ useEffect(() => {
                 {/* details fo products */}
 
                 <div className="text-sm w-full md:w-1/2 md:bg-transparent bg-white p-5">
-                    <h1 className="text-3xl md:dark:white font-medium">{product.name}</h1>
+                    <h1 className="text-3xl md:dark:text-white font-medium">{product.name}</h1>
 
                     <div className="flex items-center gap-0.5 ">
                                         {Array(5).fill('').map((_, i) => (

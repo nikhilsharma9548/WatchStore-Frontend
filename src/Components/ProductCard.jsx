04e34771564743fad80bd870367746cca44c1733
Nikhil Sharma <ns630403@gmail.com>
2025-09-20 21:35:47 +0530

    Update ProductCard.jsx

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -50,7 +50,8 @@ const ProductCard = ({product}) => {
                                 Add
                             </button>
                         ) : (
-                            <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-[#274472]/25 dark:text-gray-300/25 rounded select-none">
+                            <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] dark:text-gray-300/30  dark:border-gray-400 border border-[#274472]/60 md:w-[80px] w-[64px] h-[34px] rounded text-[#274472] font-medium cursor-pointer" onClick={() => addToCart(product._id)} >
+                                <p><IoCartOutline/></p> bg-[#274472]/25 dark:text-gray-300/25 rounded select-none">
                                 <button onClick={() => {removeFromCart(product._id)}} className="cursor-pointer text-md px-2 h-full" >
                                     -
                                 </button>

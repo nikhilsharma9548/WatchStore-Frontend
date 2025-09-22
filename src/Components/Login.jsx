c5ab934fdbd7348df28f8a91e6989edd673fb368
Nikhil Sharma <ns630403@gmail.com>
2025-09-22 14:36:39 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -69,7 +69,7 @@ const Login = () => {
                 </p>
             )}
 
-           <button className="bg-[#274472]  gap-5 flex justify-center items-center hover:bg-[#274472]/80 transition-all text-white w-full py-2 rounded-md cursor-pointer">
+           <button className="bg-[#274472] dark:bg-red-700 dark:hover:bg-red-800  gap-5 flex justify-center items-center hover:bg-[#274472]/80 transition-all text-white w-full py-2 rounded-md cursor-pointer">
                 {state === "register" ? "Create Account" : "Login"}
                 {loading && ( <div className=" border-2  aspect-square w-5 rounded-full
          border-white border-t-transparent border-r-0
